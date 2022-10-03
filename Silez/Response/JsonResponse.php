0b403612d9fed62cfc85a83c13e7fb0f68bc4543
Marc DiBlasi <marc.diblasi@gmail.com>
2022-10-03 06:54:31 -0500

    Move MIT.LICENSE to LICENSE

--- Silez/Response/JsonResponse.php
+++ Silez/Response/JsonResponse.php
@@ -5,7 +5,7 @@
  *
  * Author: Marc DiBlasi <marc.diblasi@gmail.com>
  *
- * For the full copyright and license information, please view the MIT.LICENSE
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
